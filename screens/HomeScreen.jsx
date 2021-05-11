f6110f68e12af2c8ebcad5244edc453f3a60cba9
Louis ZEGGANE <louiszeggane@gmail.com>
2021-05-12 00:51:23 +0200

    Base screen style

--- screens/HomeScreen.jsx
+++ screens/HomeScreen.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import Fire from '../Fire';
-import { View, Text, Button } from "react-native";
-import { ListItem } from 'react-native-elements/dist/list/ListItem';
+import { View } from "react-native";
+import { ListItem } from 'react-native-elements';
 
 export function HomeScreen ({ navigation }) {
   const [lists, setLists] = useState([]);
@@ -24,19 +24,13 @@ export function HomeScreen ({ navigation }) {
 
   return (
     <View>
-      {/* { !loading &&
-        lists.map((list, i) => (
-          <ListItem key={i}>
-            <ListItem.Content>
-              <ListItem.Title>{ list.name }</ListItem.Title>
-            </ListItem.Content>
-          </ListItem>
-        ))
-      } */}
-      { loading && <Text>Loading...</Text> }
       { !loading &&
         lists.map(list => (
-            <Text key={list.id} onPress={() => navigation.navigate('List', { list: list })}>{ list.name }</Text>
+          <ListItem key={list.id} onPress={() => navigation.navigate('List', { list: list })} bottomDivider>
+            <ListItem.Content>
+              <ListItem.Title style={{ color: list.color }}>{ list.name }</ListItem.Title>
+            </ListItem.Content>
+          </ListItem>
         ))
       }
     </View>
--- screens/ListScreen.jsx
+++ screens/ListScreen.jsx
@@ -1,17 +1,43 @@
 import React from 'react';
-import { View, Text, Button } from "react-native";
+import { StyleSheet, View, Text } from "react-native";
+import { ListItem, Icon } from 'react-native-elements';
 
 export function ListScreen ({ navigation, route }) {
   const { list } = route.params;
 
   return (
     <View>
-      <Button title="Go back" onPress={() => navigation.goBack()} />
+      <View style={styles.headerView}>
+        <Icon name="arrow-back-ios" type="material" color="#a9a9a9" onPress={() => navigation.goBack()} />
+        <Text style={[styles.headerTitle, { color: list.color }]}>{ list.name }</Text>
+        <Icon name="settings" type="material" color="#a9a9a9" />
+      </View>
       {
         list.todos.map(todo => (
-          <Text key={todo.id}>{ todo.title }</Text>
+          <ListItem key={todo.id} bottomDivider>
+            <ListItem.Content style={styles.todoItem}>
+              <ListItem.CheckBox checked={todo.completed} />
+              <ListItem.Title style={{ flex: 4 }}>{ todo.title }</ListItem.Title>
+              <Icon name="edit" type="material" color="#a9a9a9" />
+            </ListItem.Content>
+          </ListItem>
         ))
       }
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  headerView: {
+    flexDirection: 'row',
+    paddingBottom: 10,
+  },
+  headerTitle: {
+    fontSize: 20,
+    flex: 1,
+    textAlign: 'center',
+  },
+  todoItem: {
+    flexDirection: 'row',
+  },
+});
