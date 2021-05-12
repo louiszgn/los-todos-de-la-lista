350054e7a0e47c69d80644bb4170b19e5f109b8e
AURESANTA <auredcsanta@gmail.com>
2021-05-12 14:48:03 +0200

    branche features

--- screens/HomeScreen.jsx
+++ screens/HomeScreen.jsx
@@ -33,7 +33,6 @@ export function HomeScreen ({ navigation }) {
           </ListItem>
         ))
       }
-      <Text onPress={() => navigation.navigate('Add')}>Ajouter une liste</Text>
     </View>
   );
 };
